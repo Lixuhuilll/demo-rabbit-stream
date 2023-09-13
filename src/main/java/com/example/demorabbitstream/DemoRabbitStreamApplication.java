a9b5e788a1ae50439ec870313f1c61a307750f27
lixuhuilll <676824363@qq.com>
2023-09-13 12:20:00 +0800

    按照 Artem Bilan 的方法，成功连接上了 RabbitMQ Stream。

--- src/main/java/com/example/demorabbitstream/DemoRabbitStreamApplication.java
+++ src/main/java/com/example/demorabbitstream/DemoRabbitStreamApplication.java
@@ -1,10 +1,14 @@
 package com.example.demorabbitstream;
 
+import com.rabbitmq.stream.Address;
+import com.rabbitmq.stream.Message;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.EnvironmentBuilderCustomizer;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.context.annotation.Bean;
 
 @Slf4j
 @SpringBootApplication
@@ -12,6 +16,13 @@ public class DemoRabbitStreamApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoRabbitStreamApplication.class, args);
+    }
+
+    @Bean
+    EnvironmentBuilderCustomizer environmentBuilderCustomizer(RabbitProperties rabbitProperties) {
+        RabbitProperties.Stream rabbitStreamProperties = rabbitProperties.getStream();
+        return builder -> builder
+                .addressResolver(add -> new Address(rabbitStreamProperties.getHost(), rabbitStreamProperties.getPort()));
     }
 
     @RabbitListener(id = "consumer", queues = "stream.test")
